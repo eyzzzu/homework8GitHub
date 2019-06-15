ab2b4b183db79460888dc96414edbafe22dc566d
eyzzzu <eyzzzu@gmail.com>
2019-06-15 17:42:25 +0300

    Add method someText

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         test();
+        someText();
+    }
+
+    private static void someText() {
+        System.out.println("someText");
     }
 
     private static void test() {
