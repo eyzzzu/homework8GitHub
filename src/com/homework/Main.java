b98f437fd711f314bdf886126b196b3488e34b5f
eyzzzu <eyzzzu@gmail.com>
2019-06-15 17:23:59 +0300

    homework8 Basic commit

---
+++ src/com/homework/Main.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        test();
+    }
+
+    private static void test() {
+        System.out.println("test");
+    }
+
+}
